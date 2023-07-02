249b2f8d8659d716a4fa1ed0f6296395c8d56492
RadSparkle <wwwartg1@naver.com>
2023-07-03 01:14:48 +0900

    Initial commit

---
+++ src/main/java/com/stock/stock_info_web/StockInfoWebApplication.java
@@ -0,0 +1,13 @@
+package com.stock.stock_info_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockInfoWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockInfoWebApplication.class, args);
+    }
+
+}
